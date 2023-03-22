34eb25735e4c2ce3d5eface20a3461763cae570d
Pedro Gomes <up202108825@up.pt>
2023-03-22 15:36:56 +0000

    implemented bfs

--- src/Search.cpp
+++ src/Search.cpp
@@ -1,5 +1,6 @@
 #include "Network.h"
 #include <stack>
+#include <queue>
 using namespace std;
 
 void Network::dfs(string& s){
@@ -20,6 +21,28 @@ void Network::dfs(string& s){
         for(auto connection : this->stations[current].getConnections()){
             if(!this->stations[connection.getDestination()].isVisited())
                 stack.push(connection.getDestination());
+        }
+    }
+}
+
+void Network::bfs(std::string &s) {
+    queue<string> q;
+    string current;
+    for(auto station: this->stations)
+        station.second.setVisited(false);
+
+    this->stations[s].setVisited(true);
+    q.push(s);
+
+    while(!q.empty()){
+        current = q.front();
+        q.pop();
+
+        for(auto station: this->stations[current].getConnections()){
+            if(!this->stations[station.getDestination()].isVisited()){
+                this->stations[station.getDestination()].setVisited(true);
+                q.push(station.getDestination());
+            }
         }
     }
 }
